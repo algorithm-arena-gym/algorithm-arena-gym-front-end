5100971a8930f1687e94028470dae2b1d16b44a8
phukyada <phukyada1234567@gmail.com>
2023-04-06 19:56:33 +0700

    memC

--- src/pages/member/createMember.tsx
+++ src/pages/member/createMember.tsx
@@ -290,33 +290,10 @@ export default function createMember() {
                                         </div>
 
                                         
-                                        {/* <div className="basis-1/3 flex justify-start ... ">
-                                            <div>
-                                                <div className="grid ">
-                                                    <label htmlFor="first-name" className="font-light text-base ">Time</label>
-                                                    <div className="mt-2 mr-20">
-                                                        <select
-                                                            id="country"
-                                                            name="country"
-                                                            autoComplete="off"
-                                                            className="font-semibold text-xl rounded-md block w-full "
-                                                        >
-                                                            <option>10-11</option>
-                                                            <option>11-12</option>
-                                                            <option>12-13</option>
-                                                            <option>13-14</option>
-                                                            <option>14-15</option>
-                                                            <option>15-16</option>
-                                                            <option>16-17</option>
-                                                            <option>17-18</option>
-                                                            <option>18-19</option>
-                                                            <option>19-20</option>
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>*/}
+                                        
                                     </div>
+                                    <div className=" text-black bg-[#FFFFFF] text-xl rounded-md  ml-20 mr-5 mt-3  flex justify-center">+</div>
+
 
                                 </div>
                                 <div className="basis-1/2">
@@ -382,6 +359,8 @@ export default function createMember() {
                                             </div>
                                         </div>
                                     </div>
+                                      <div className=" text-black bg-[#FFFFFF] text-xl rounded-md   mr-20 mt-3  flex justify-center">+</div>
+
 
                                 </div>
                             </div>
