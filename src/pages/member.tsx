fdd69f52e9bc35aa5033d97041be3b3bbe07142f
baemehyun <thezch00@gmail.com>
2023-04-19 11:23:27 +0700

    rank all
    done

--- src/pages/member.tsx
+++ src/pages/member.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import { useRouter } from "next/router";
 import 'src/app/globals.css'
 import TabNavbar from "src/app/navbar/navbar.js";
 import "src/app/rankcard.css";
@@ -25,11 +26,13 @@ interface Rank {
   rankName: string,
   rankDetail: string,
   rankPic: string
-  rankPrice:number
+  rankPrice: number
 }
 
 export default function allMember() {
-  const [memberData, setData] = useState<Member | null>(null);
+  const router = useRouter();
+  const [memberData, setMemberData] = useState<Array<Member> | null>(null);
+  const [rankData, setRankData] = useState<Array<Rank> | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
@@ -42,17 +45,22 @@ export default function allMember() {
       try {
         const res = await fetch('http://localhost:4000/member');
         const json = await res.json();
-        setData(json);
+        setMemberData(json);
+        const res2 = await fetch('http://localhost:4000/rank');
+        const json2 = await res2.json();
+        setRankData(json2);
         setError(null);
+        
+
+
       } catch (error) {
         console.error(error);
         setError('An error occurred while fetching the data.');
-        setData(null);
+        setMemberData(null);
       }
 
       setLoading(false);
     }
-
     fetchData();
   }, []);
 
@@ -67,8 +75,13 @@ export default function allMember() {
   if (!memberData) {
     return <div>No data to display.</div>;
   }
+  // if (!rankData) {
+  //   return <div>No rankData to display.</div>;
+  // }
 
   const members = JSON.parse(JSON.stringify(memberData));
+  // const ranks = JSON.parse(JSON.stringify(rankData));
+  // console.log(ranks);
 
 
   return (
@@ -77,29 +90,30 @@ export default function allMember() {
         <TabNavbar />
       </div>
       <div className='grid gap-6 grid-cols-3 place-content-center md:px-20 pt-10'>
-        
-          {members.map((member: Member) => (
-            <div className='  bg-[#D9D9D9] p-4 rounded-3xl  w-70 '>
-              <Link href={`/member/${member.memberID}`}>
-                <div className='flex flex-row '> 
-                  <img className='flex border-[#E2FEA7] border-4 w-20 h-20 rounded-full' src={member.profilePic} alt="profilePicture" />
-                  <div className='pl-4 '>
 
-                    <p>{member.nameEng}</p>
-                    {/* need rank */}
-                    {}
-                    <p>{member.phone}</p>
-                  </div>
+        {members.map((member: Member, index: number) => (
+          <div className='  bg-[#D9D9D9] p-4 rounded-3xl  w-70 '>
+            <Link href={`/member/${member.memberID}`}>
+              <div className='flex flex-row '>
+                <img className='flex border-[#E2FEA7] border-4 w-20 h-20 rounded-full' src={member.profilePic} alt="profilePicture" />
+                <div className='pl-4 '>
+
+                  <p>{member.nameEng}</p>
+                  {/* need rank */}
+                  <p className="font-semibold text-xl">{rankData?.find(rank => rank.rankID===member.rankID)?.rankName}</p>
+                  { }
+                  <p>{member.phone}</p>
                 </div>
-              </Link>
-            </div>
-          ))}
-          <div className=' bg-[#D9D9D9] p-4 rounded-3xl  w-70 h-28 grid ' >
-            <div className='grid justify-items-center'>
-            <button type="button" className="bttn" >+</button>
-            </div>
+              </div>
+            </Link>
           </div>
-        
+        ))}
+        <div className=' bg-[#D9D9D9] p-4 rounded-3xl  w-70 h-28 grid ' >
+          <div className='grid justify-items-center'>
+            <button type="button" className="bttn" >+</button>
+          </div>
+        </div>
+
 
       </div>
     </body>
